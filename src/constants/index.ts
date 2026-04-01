9781d899569f66e9216f68a00b7616cb0d01f3d3
BaseUSDP <baseusdp@proton.me>
2026-04-01 19:22:00 -0400

    Add constants barrel export file
    Co-Authored-By: BaseUSDP Team <baseusdp@proton.me>

---
+++ src/constants/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Central export for all BaseUSDP constants
+ */
+
+export * from './chains';
+export * from './tokens';
+export * from './contracts';
+export * from './routes';
+export * from './errors';
+export * from './privacy';
